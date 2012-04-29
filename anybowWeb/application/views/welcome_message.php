c5d5a70d929c8933ff2d822c0bbfb32bba0d856c
anybow <syber@tom.com>
2012-04-30 00:13:42 +0800

    test chinese

--- anybowWeb/application/views/welcome_message.php
+++ anybowWeb/application/views/welcome_message.php
@@ -71,7 +71,7 @@
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-		<p>ÎÒÒ²Òª²âÊÔÒ»ÏÂ¿´¿´</p>
+		<p>æˆ‘ä¹Ÿè¦æµ‹è¯•ä¸€ä¸‹çœ‹çœ‹</p>
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
