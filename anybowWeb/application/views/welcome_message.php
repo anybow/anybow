862916f0e0e670daf3717971ade3b002059363ad
anybow <syber@tom.com>
2012-04-30 00:10:19 +0800

    test commit
    just test commit

--- anybowWeb/application/views/welcome_message.php
+++ anybowWeb/application/views/welcome_message.php
@@ -71,7 +71,7 @@
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
+		<p>我也要测试一下看看</p>
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
